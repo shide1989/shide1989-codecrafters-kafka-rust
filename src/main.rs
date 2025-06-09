3bff9da62a9a10978f58eb6c1b16e6083dec4802
Sebastien Hideux <hideux.sebastien@gmail.com>
2025-06-09 09:23:38 -0500

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,46 +9,62 @@ use std::{
     io::Write,
     net::{TcpListener, TcpStream},
 };
-
+const MESSAGE_SIZE_BYTES: usize = 4;
+// Request message size header
 fn handle_connection(mut stream: TcpStream) {
     println!("accepted new connection");
-    // For now assume we read only the first 12 bytes.
-    let capacity: usize = 12;
+    // stream
+    //     .set_nonblocking(true)
+    //     .expect("set_nonblocking call failed");
 
-    let mut buf = BytesMut::zeroed(capacity);
+    let mut buf: BytesMut = BytesMut::zeroed(MESSAGE_SIZE_BYTES);
+    // Read the message size header
+    while let Ok(len) = stream.peek(&mut buf) {
+        if len == 0 {
+            println!("No data left to read.");
+            break;
+        }
+        let bytes: usize = buf.get_i32() as usize;
+        buf = BytesMut::zeroed(MESSAGE_SIZE_BYTES); // reset
+        println!("Bytes read {len}: {bytes}");
+        let mut buf: BytesMut = BytesMut::zeroed(bytes + MESSAGE_SIZE_BYTES);
+        if let Err(e) = stream.read(&mut buf) {
+            eprintln!("Error reading from stream: {e}");
+            return;
+        }
 
-    if let Err(e) = stream.read(&mut buf) {
-        eprintln!("Error reading from stream: {e}");
-        return;
+        let req = Request::try_from(buf.freeze()).expect("Request should have been parsed.");
+
+        println!("Parsed request: {req:?}");
+
+        let error_code = if req.request_api_version == 4 {
+            0
+        } else {
+            UNSUPPORTED_VERSION_CODE
+        };
+
+        let res = Response {
+            correlation_id: req.correlation_id,
+            error_code,
+            api_versions: API_VERSIONS,
+        };
+        let buf = res.to_bytes();
+
+        println!("Sending response {res:?}");
+        println!("Sending buffer {buf:?}");
+        stream
+            .write_all(&buf)
+            .expect("Stream should have been written.");
+        stream.flush().unwrap();
+        println!("Stream written.");
     }
-    let req = Request::try_from(buf.freeze()).expect("Request should have been parsed.");
-
-    println!("Parsed request: {req:?}");
-
-    let error_code = if req.request_api_version == 4 {
-        0
-    } else {
-        UNSUPPORTED_VERSION_CODE
-    };
-
-    let res = Response {
-        correlation_id: req.correlation_id,
-        error_code,
-        api_versions: API_VERSIONS,
-    };
-    let buf = res.to_bytes();
-
-    println!("Sending response {res:?}");
-    println!("Sending buffer {buf:?}");
-    stream.write_all(&buf).unwrap();
-    stream.flush().unwrap();
-    stream.shutdown(std::net::Shutdown::Both).unwrap();
-
-    println!("Stream written.")
+    stream
+        .shutdown(std::net::Shutdown::Both)
+        .expect("Stream should be connected");
 }
 
 fn main() -> std::io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:9092").unwrap();
+    let listener = TcpListener::bind("127.0.0.1:9092")?;
 
     for stream in listener.incoming() {
         match stream {
